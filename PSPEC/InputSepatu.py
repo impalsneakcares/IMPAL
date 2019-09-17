de72af04edbaaba380de08b0cadc978c3bc17f24
Jeddie Salam <54430706+jeddiesaudi@users.noreply.github.com>
2019-09-17 20:32:15 +0700

    Add files via upload

---
+++ PSPEC/InputSepatu.py
@@ -0,0 +1,21 @@
+print("====================================")
+print("	   LAYANAN SEPATU    ")
+print("Bronze: 20k (3 hari)  ")
+print("Silver: 30k (2 hari)  ")
+print("Gold  : 35k (1 hari)  ")
+print("====================================")
+mrk = str(input("Masukkan Merk Sepatu: "))
+wrn = str(input("Masukkan Warna Sepatu: "))
+lyn = str(input("Pilih layanan: "))
+
+if(lyn == "Bronze" or lyn == "bronze"):
+	print("Terimakasih, pesanan anda akan segera kami proses!")
+elif(lyn == "Silver" or lyn == "silver"):
+	print("Terimakasih, pesanan anda akan segera kami proses!")
+elif(lyn == "Gold" or lyn == "gold"):
+	print("Terimakasih, pesanan anda akan segera kami proses!")
+else:
+	print("Maaf, kami hanya memiliki 3 layanan")
+
+
+
