b0f04bad38ea306f76f4258b3221758ff8e17c00
Jeddie Salam <54430706+jeddiesaudi@users.noreply.github.com>
2019-09-17 20:43:14 +0700

    Update InputSepatu.py

--- PSPEC/InputSepatu.py
+++ PSPEC/InputSepatu.py
@@ -9,11 +9,14 @@ wrn = str(input("Masukkan Warna Sepatu: "))
 lyn = str(input("Pilih layanan: "))
 
 if(lyn == "Bronze" or lyn == "bronze"):
-	print("Terimakasih, pesanan anda akan segera kami proses!")
+	print("Terimakasih, pesanan anda akan segera kami proses!\n")
+	print("Sepatu anda dengan merk "+mrk+" berwarna "+wrn+" dengan layanan "+lyn)
 elif(lyn == "Silver" or lyn == "silver"):
-	print("Terimakasih, pesanan anda akan segera kami proses!")
+	print("Terimakasih, pesanan anda akan segera kami proses!\n")
+	print("Sepatu anda dengan merk "+mrk+" berwarna "+wrn+" dengan layanan "+lyn)
 elif(lyn == "Gold" or lyn == "gold"):
-	print("Terimakasih, pesanan anda akan segera kami proses!")
+	print("Terimakasih, pesanan anda akan segera kami proses!\n")
+	print("Sepatu anda dengan merk "+mrk+" berwarna "+wrn+" dengan layanan "+lyn)
 else:
 	print("Maaf, kami hanya memiliki 3 layanan")
 
